79824b1fca4f5b90022ada96e241a346f5e26af2
viktor <www.pista43@centrum.sk>
2026-09-23 11:33:19 +0200

    Uvodna hodina - moj prvy kod

---
+++ uloha1.py
@@ -0,0 +1,6 @@
+
+x = 10      # vytvoril som premennu x a pridal som jej hodnotu 10
+print(x)    # pomocou prikazu print som vypisal hodnotu ulozenu v premennej x
+
+x = x + 8   # povodnu premennu x som prepisal tak ze som k nej pripocital hodnotu 8
+print(x)    # vypisal som premennu x
